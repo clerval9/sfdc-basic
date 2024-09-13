0fe6eb522d9f08292aa59ad969d79f5ef03600d7
Val Fokle <clerval9@gmail.com>
2024-09-12 19:18:05 -0500

    lwc test

---
+++ force-app/main/default/lwc/testComp/testComp.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+import template1 from "./template1.html";
+import template2 from "./template2.html";
+
+export default class TestComp extends LightningElement {
+
+    render() {
+        return template2;
+    }
+
+}
---
+++ force-app/main/default/lwc/theLWC/theLWC.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class TheLWC extends LightningElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+
+    }
+
+    renderedCallback() {
+
+    }
+}
