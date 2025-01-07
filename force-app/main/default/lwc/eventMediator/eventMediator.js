518c66c5447c68174fcb44178ce8b0d2989b1445
Clerval Fokle <clerval9@gmail.com>
2025-01-06 20:16:20 -0800

    interview prep

---
+++ force-app/main/default/lwc/eventMediator/eventMediator.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class EventMediator extends LightningElement {
+  constructor(){}
+
+  connectedCallback(){}
+
+  disconnectedCallback(){}
+
+  renderedCallback(){}
+  
+  errorCallback(){}
+}
